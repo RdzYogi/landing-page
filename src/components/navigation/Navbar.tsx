e46889d5873039209bad8ff0dae1ce0db204a523
Silviu <silviurosu1988@gmail.com>
2023-02-20 10:48:40 +0100

    changed nav style

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -5,11 +5,11 @@ type Props ={
 
 function Navbar(props:Props) {
   return (
-    <div className='bg-gray-500 h-20 flex justify-center'>
-      <div className='flex justify-between w-1/2 text-lg'>
-        <button id="about" onClick={props.handleNavigation} className='border border-black h-12 '>About</button>
-        <button id="projects" onClick={props.handleNavigation} className='border border-black h-12'>Projects</button>
-        <button id="third" onClick={props.handleNavigation} className='border border-black h-12'>third</button>
+    <div className='h-20 flex justify-center md:w-3/4 m-auto border-b-2 border-black pb-2 w-full'>
+      <div className='flex justify-between items-end md:w-3/4 w-full mx-8 md:mx-0 text-lg font-bold'>
+        <button id="about" onClick={props.handleNavigation} className=' bg-white h-12 transition transition-duration-300 ease-in-out hover:text-gray-400'>About</button>
+        <button id="projects" onClick={props.handleNavigation} className='bg-white h-12 transition transition-duration-300 ease-in-out hover:text-gray-400'>Projects</button>
+        <button id="third" onClick={props.handleNavigation} className='bg-white h-12 transition transition-duration-300 ease-in-out hover:text-gray-400'>third</button>
       </div>
     </div>
   )
