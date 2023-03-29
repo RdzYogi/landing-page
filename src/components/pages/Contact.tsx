7100bb6b0b0e0682f1c0ea5d0f5870fa4508c356
Silviu <silviurosu1988@gmail.com>
2023-03-29 10:52:43 +0200

    mail working

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,23 +1,64 @@
-import React from 'react'
+
+import emailjs from '@emailjs/browser'
+import React, { useEffect } from 'react'
+
 
 function Contact() {
+  const service = 'service_dou4ss6'
+  const template = 'template_5o8baun'
+  const publicKey = 'U9R08FW5PBGMt_iiy'
+  const [formInfo, setFormInfo] = React.useState({
+    email: '',
+    subject: '',
+    message: '',
+  })
+  useEffect(() => {
+
+  }, [])
+  const handleEmail = (e: any) => {
+    setFormInfo({
+      ...formInfo,
+      email: e.target.value
+    })
+  }
+  const handleSubject = (e: any) => {
+    setFormInfo({
+      ...formInfo,
+      subject: e.target.value
+    })
+  }
+  const handleMessage = (e: any) => {
+    setFormInfo({
+      ...formInfo,
+      message: e.target.value
+    })
+  }
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log(formInfo)
+    emailjs.send(service, template, formInfo, publicKey)
+    .then((response) => {
+        console.log(response.text);
+    })
+  }
+
   return (
     <div className='text-gray-200 z-20 w-full md:w-3/4 lg:w-1/2 m-auto relative h-fit '>
       <section className="">
       <div className="pb-2 px-4 mx-auto max-w-screen-md">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">Contact me</h2>
-          <form action="#" className="space-y-8">
+          <form onSubmit={handleSubmit} className="space-y-8">
               <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-300">Your email</label>
-                  <input type="email" id="email" className="shadow-sm border text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light" placeholder="name@mail.com" required/>
+                  <input onChange={handleEmail} type="email" id="email" className="shadow-sm border text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light" placeholder="name@mail.com" required/>
               </div>
               <div>
                   <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-300">Subject</label>
-                  <input type="text" id="subject" className="block p-3 w-full text-sm rounded-lg border shadow-sm focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light" placeholder="Let me know what it is about" required/>
+                  <input onChange={handleSubject} type="text" id="subject" className="block p-3 w-full text-sm rounded-lg border shadow-sm focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light" placeholder="Let me know what it is about" required/>
               </div>
               <div className="sm:col-span-2">
                   <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-400">Your message</label>
-                  <textarea id="message" rows={6} className="block p-2.5 w-full text-sm rounded-lg shadow-sm border focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500" placeholder="Type a message..."></textarea>
+                  <textarea onChange={handleMessage} id="message" rows={6} className="block p-2.5 w-full text-sm rounded-lg shadow-sm border focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500" placeholder="Type a message..."></textarea>
               </div>
               <button type="submit" className="py-3 px-5 text-sm font-medium border border-gray-500 hover:border-gray-300 hover:text-gray-200 text-center text-white rounded-lg transition duration-300 ease-in-out sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-800">Send message</button>
           </form>
