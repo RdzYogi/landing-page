13082a8225fab057b4a81619c9d3d549f8093f34
Silviu <silviurosu1988@gmail.com>
2023-03-31 15:37:23 +0200

    about me changed

--- src/components/about/AboutMe.tsx
+++ src/components/about/AboutMe.tsx
@@ -12,7 +12,7 @@ function AboutMe() {
       <div id="avatar" className='aspect-square w-20 mb-5 hidden md:flex' style={{backgroundImage: `url(${profile})`, backgroundSize: "cover",backgroundPosition:"center center"}}></div>
       <div className='md:flex md:flex-col justify-between h-fit w-full'>
         <h1 className=' text-gray-600 '>{"<helloWorld>"}</h1>
-        <h1 className='text-sm ml-20 text-gray-200'>I am a passionate developer that discovered his passion for web after finishing LeWagon coding bootcamp.</h1>
+        <h1 className='text-sm ml-20 text-gray-200'>A graduate of Le Wagon coding bootcamp, my passion lies in web development. Functional and beautiful websites that solve real-world problems drive my enthusiasm for this craft. I spend a significant amount of time perfecting my programming languages and design skills, eager to create amazing things. With my expertise, I am thrilled to work alongside other developers and collaborate to bring innovative solutions to life.</h1>
         <div className='flex'>
           <a className='ml-20 mt-3 py-2 px-2 text-sm font-medium border border-gray-500 hover:border-gray-300 hover:text-gray-200 text-center text-white rounded-lg transition duration-300 ease-in-out sm:w-fit' href="https://drive.google.com/file/d/1CQarj_PpIHsFjvoyH2YiD6GKMHF6ZmW5/view?usp=sharing" target="blank"> CV</a>
           <button onClick={handleClick} className="ml-5 mt-3 py-2 px-2 text-sm font-medium border border-gray-500 hover:border-gray-300 hover:text-gray-200 text-center text-white rounded-lg transition duration-300 ease-in-out sm:w-fit">Contact me</button>
