41c8719b22485fe33323e380dd8419ed77d9882e
Silviu <silviurosu1988@gmail.com>
2023-04-03 19:14:42 +0200

    class select

--- src/components/game/UI.tsx
+++ src/components/game/UI.tsx
@@ -1,17 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import warriorPortrait from '../../assets/game/warrior/portrait.png'
+import wizardPortrait from '../../assets/game/wizard/portrait.png'
 
 function UI() {
+  const [player, setPlayer] = useState("")
+
+  useEffect(() => {
+    const player = window.localStorage.getItem('player')
+    console.log(player)
+    if (player === null){
+      const mainGame = document.getElementById('mainGame')
+      const menu = document.getElementById('menu')
+      if (mainGame === null || menu === null) return
+      mainGame.classList.add('hidden')
+      menu.classList.remove('hidden')
+    } else {
+      const mainGame = document.getElementById('mainGame')
+      const menu = document.getElementById('menu')
+      if (mainGame === null || menu === null) return
+      mainGame.classList.remove('hidden')
+      setPlayer(player)
+    }
+  }, [])
+
+  const pickWarrior = () => {
+    const mainGame = document.getElementById('mainGame')
+    const menu = document.getElementById('menu')
+    if (mainGame === null || menu === null) return
+    mainGame.classList.remove('hidden')
+    menu.classList.add('hidden')
+    setPlayer('warrior')
+    window.localStorage.setItem('player', 'warrior')
+  }
+  const pickWizard = () => {
+    const mainGame = document.getElementById('mainGame')
+    const menu = document.getElementById('menu')
+    if (mainGame === null || menu === null) return
+    mainGame.classList.remove('hidden')
+    menu.classList.add('hidden')
+    setPlayer('wizard')
+    window.localStorage.setItem('player', 'wizard')
+  }
+  const newGame = () => {
+    window.localStorage.removeItem('player')
+    const mainGame = document.getElementById('mainGame')
+    const menu = document.getElementById('menu')
+    if (mainGame === null || menu === null) return
+    mainGame.classList.add('hidden')
+    menu.classList.remove('hidden')
+  }
   return (
     <div>
-      <div id="info-bar" className='w-full h-10 bg-blue-500 mb-10'>
-      </div>
-      <div className='battle w-full h-[20vh] bg-gray-200 flex justify-around items-end my-20'>
-        <div id="player" className='bg-green-200 w-40 h-full' >
+      <div id="menu" className='hidden h-[60vh] w-full '>
+        <h1 className='text-center pt-10'>Pick Your Class</h1>
+        <div className='flex justify-around mt-10'>
+          <div className='flex flex-col items-center'>
+            <h1>Warrior</h1>
+            <img onClick={pickWarrior} src={warriorPortrait} alt="warriorPortrait" className='w-44 cursor-pointer'/>
+          </div>
+          <div className='flex flex-col items-center'>
+            <h1>Wizard</h1>
+            <img onClick={pickWizard} src={wizardPortrait} alt="warriorPortrait" className='w-44 cursor-pointer'/>
+          </div>
+
         </div>
-        <div id="enemy" className='bg-red-200 w-40 h-full'>
-        </div>
       </div>
-      <div id='action-bar' className='w-full h-32 bg-yellow-400 mt-10'>
+      <div id="mainGame" className='hidden'>
+        <div id="info-bar" className='w-full h-10 bg-blue-500 mb-10'>
+          <button onClick={newGame}>New Game</button>
+        </div>
+        <div className='battle w-full h-[20vh] bg-gray-200 flex justify-around items-end my-20'>
+          <div id="player" className='bg-green-200 w-40 h-full' >
+          </div>
+          <div id="enemy" className='bg-red-200 w-40 h-full'>
+          </div>
+        </div>
+        <div id='action-bar' className='w-full h-32 bg-yellow-400 mt-10'>
+        </div>
 
       </div>
     </div>
--- src/components/pages/Game.tsx
+++ src/components/pages/Game.tsx
@@ -3,7 +3,7 @@ import UI from '../game/UI'
 
 function Game() {
   return (
-    <div className='text-white z-20 w-full md:w-3/4 lg:w-1/2 m-auto relative h-fit medieval'>
+    <div className='text-white z-20 w-full md:w-3/4 m-auto relative h-fit medieval'>
       <UI/>
     </div>
   )
