03b9cdf6a353f9bd44e3d3fbe48fe0d3ed3a1caa
Silviu <silviurosu1988@gmail.com>
2023-04-03 19:25:37 +0200

    health sistem

--- src/components/game/Player.tsx
+++ src/components/game/Player.tsx
@@ -3,14 +3,16 @@ import wizardPortrait from '../../assets/game/wizard/portrait.png'
 import warriorPortrait from '../../assets/game/warrior/portrait.png'
 
 function Player({player, damage}: {player: string, damage: number | 0}) {
-  const [health, setHealth] = useState(100)
   const [portrait, setPortrait] = useState("")
+  const [health, setHealth] = useState(0)
 
   useEffect(() => {
     if (player === "warrior") {
       setPortrait(warriorPortrait)
+      setHealth(100)
     } else if (player === "wizard") {
       setPortrait(wizardPortrait)
+      setHealth(75)
     }
   }, [player, damage])
   return (
