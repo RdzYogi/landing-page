ce3912ba313dc84ef21dc6ea80f8a1853a26de2c
Silviu <silviurosu1988@gmail.com>
2023-04-04 19:47:37 +0200

    dev mode

--- src/components/pages/Game.tsx
+++ src/components/pages/Game.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import UI from '../game/UI'
 
 function Game() {
+  const visible = false
   return (
     <div className='text-white z-20 w-full md:w-3/4 m-auto relative h-fit medieval'>
-      <UI/>
+      {visible ? <UI/> : <p className='text-center'>Coming soon</p>}
     </div>
   )
 }
