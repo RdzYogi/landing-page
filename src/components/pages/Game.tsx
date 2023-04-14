d2e22b949533f5c3128a159b3189f5d18a9a3acc
Silviu <silviurosu1988@gmail.com>
2023-04-14 10:08:47 +0200

    store scope reduced to game

--- src/components/pages/Game.tsx
+++ src/components/pages/Game.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import UI from '../game/UI'
+import { Provider } from 'react-redux'
+import { store } from '../redux/store/store'
 
 
 function Game() {
   const visible = false
   return (
-    <div className='text-white z-20 w-full md:w-3/4 m-auto relative h-fit medieval'>
-      {visible ? <UI/> : <p className='text-center'>Coming soon</p>}
-    </div>
+    <Provider store={store}>
+      <div className='text-white z-20 w-full md:w-3/4 m-auto relative h-fit medieval'>
+        {visible ? <UI/> : <p className='text-center'>Coming soon</p>}
+      </div>
+    </Provider>
   )
 }
 
