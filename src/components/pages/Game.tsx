dcc075d68e0d82228ff72f3b92c9fab82d00f0d0
Silviu <silviurosu1988@gmail.com>
2023-04-14 13:20:40 +0200

    set to hidden

--- src/components/pages/Game.tsx
+++ src/components/pages/Game.tsx
@@ -4,7 +4,7 @@ import UI from '../game/UI'
 
 
 function Game() {
-  const visible = true
+  const visible = false
   return (
       <div className='text-white z-20 w-full md:w-3/4 m-auto relative h-fit medieval'>
         {visible ? <UI/> : <p className='text-center'>Coming soon</p>}
