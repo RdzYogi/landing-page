9e75d3438139759ce87773615a98780c88131248
Silviu <silviurosu1988@gmail.com>
2023-04-24 20:27:59 +0200

    player block displays correctly

--- src/components/game/EnemyStatusBar.tsx
+++ src/components/game/EnemyStatusBar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function EnemyStatusBar() {
+  const enemyBlock = useSelector((state: any) => state.enemy.block)
   return (
     <div>EnemyStatusBar</div>
   )
