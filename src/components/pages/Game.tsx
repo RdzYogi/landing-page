ff4a603a5f6aee5985cfab3b695b7741e691d399
Silviu <silviurosu1988@gmail.com>
2023-05-03 09:47:38 +0200

    added form for game access code

--- src/components/pages/Game.tsx
+++ src/components/pages/Game.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import UI from '../game/UI'
 
 
 function Game() {
   const visible = false
-  const [Ui , setUi] = React.useState(<div></div>)
+  const [Ui , setUi] = useState(<div></div>)
+
   useEffect(() => {
     if(Ui){
       setUi(<UI/>)
@@ -12,9 +13,20 @@ function Game() {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  const handleChange = (e: any) => {}
   return (
-      <div className='text-white z-20 w-full md:w-[90%] m-auto relative h-fit medieval'>
-        {visible ? Ui : <p className='text-center'>Coming soon</p>}
+      <div className='text-white z-20 w-full md:w-[90%] m-auto flex flex-col h-fit medieval'>
+        {visible ? Ui :
+        <>
+          <div className='w-40'>
+              <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-300">Enter code for pre-alpha</label>
+              <input onChange={handleChange} type="text" id="subject" className="block p-3 w-full text-sm rounded-lg border shadow-sm focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light" placeholder="Code:" required/>
+          </div>
+          <button type="submit" className={"py-3 px-5 text-sm font-medium border border-gray-500 hover:border-gray-300 hover:text-gray-200 text-center rounded-lg transition duration-300 ease-in-out sm:w-fit flex "}>
+            Play
+          </button>
+        </>
+      }
       </div>
 
   )
